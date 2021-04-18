c454f658ed7f2deb7cf254361f83e61299c87f6c
ern0 <ern0@linkbroker.hu>
2021-04-18 16:47:02 +0200

    Lang: moved prefix to postfix, refactor of Text

--- Text.php
+++ Text.php
@@ -4,39 +4,65 @@ Class Text {
 	function __construct($fnam) {
 		$this->fileName = $fnam;
 		$this->lines = [];
+		$this->base = $this;
 	} // ctor()
 
 
-	function load(&$lines = null) {
+	function addLine($line) {
+		$this->base->lines[] = $line;
+	} // addLine()
 
-		if ($lines == null) $lines = &$this->lines;
+
+	function load() {
 		
 		$file = fopen($this->fileName,"r");
+		if (!$file) fatal("file open error: " . $this->fileName);
 		while (!feof($file)) {
 
 			$line = rtrim(fgets($file));
 			if ($line == "") continue;
-			if ($this->checkAndProcessIncludeCommand($line)) continue;
+			if ($this->checkAndProcessCommand($line)) continue;
 
-			$lines[] = $line;
+			$this->addLine($line);
 
 		} // while not eof
 
 	} // load()
 
 
-	function checkAndProcessIncludeCommand($line) {
+	function checkAndProcessCommand($line) {
 
 		if (ord($line[0]) <= 32) return false;
 
 		$a = explode(" ",$line);
-		if ($a[0] != "include") return false;
+		if ($a[0] == "include") {
+			$this->processIncludeCommand($a[1]);
+			return true;
+		}
 
-		$inc = new Text($a[1]);
-		$inc->load($this->lines);
+		if ($a[0] == "node") {
+			$this->processNodeCommand($line);
+			return true;
+		}
 
-		return true;
-	} // checkAndProcessIncludeCommand()
+		return false;
+	} // checkAndProcessCommand()
+
+
+	function processIncludeCommand($fnam) {
+
+		$inc = new Text($fnam);
+		$inc->base = $this;
+		$inc->load();
+
+	} // processIncludeCommand()
+
+
+	function processNodeCommand($line) {
+
+		$this->addLine($line);
+
+	} // processNodeCommand()
 
 
 	function dump() {
